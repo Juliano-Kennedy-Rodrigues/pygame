305773bf0f9377bb821311ce8d04c55ee8b60285
Juliano Rodrigues <julianokennedy11@gmail.com>
2024-06-26 20:53:35 -0300

    criado entityFactory, desenvolvida classe background e criado background do Leovel1

--- CrabShooter/pythonProject1/code/Const.py
+++ CrabShooter/pythonProject1/code/Const.py
@@ -11,3 +11,14 @@ MENU_OPTION = ('NEW GAME 1P',
 # C
 COLOR_WHITE =(255, 255, 255)
 COLOR_YELLOW = (255, 255, 128)
+
+# BG
+BG_ARRAY = ['Level1Bg0', 'Level1Bg1', 'Level1Bg2', 'Level1Bg3', 'Level1Bg4']
+
+# E
+ENTITY_SPEED = {'Level1Bg0': 0,
+                'Level1Bg1': 1,
+                'Level1Bg2': 2,
+                'Level1Bg3': 3,
+                'Level1Bg4': 4
+                }
--- CrabShooter/pythonProject1/code/Level.py
+++ CrabShooter/pythonProject1/code/Level.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+
+import pygame
+
+from code.Entity import Entity
+from code.EntityFactory import EntityFactory
+
 
 class Level:
     def __init__(self, window, menu_option, name):
-        self.window = None
-        self.entity_list = None
-        self.name = None
+        self.window = window
+        self.name = name
+        self.mode = menu_option  # opção do menu
+        self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
     def run(self, ):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print('loop end')
+                    pygame.quit()
+                    sys.exit()
         pass
 
