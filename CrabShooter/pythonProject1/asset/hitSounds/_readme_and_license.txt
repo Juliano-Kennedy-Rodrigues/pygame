0533de5002c83f0e055e0fbfaabcac510a7d2faa
Juliano Rodrigues <julianokennedy11@gmail.com>
2024-06-30 17:53:25 -0300

    add som fase1, 2 players, 2 inimigos, movimento

---
+++ CrabShooter/pythonProject1/asset/hitSounds/_readme_and_license.txt
@@ -0,0 +1,37 @@
+Sound pack downloaded from Freesound
+----------------------------------------
+
+"Self made trap samples"
+
+This pack of sounds contains sounds by the following user:
+ - yellobus ( https://freesound.org/people/yellobus/ )
+
+You can find this pack online at: https://freesound.org/people/yellobus/packs/41344/
+
+
+Licenses in this pack (see below for individual sound licenses)
+---------------------------------------------------------------
+
+Creative Commons 0: http://creativecommons.org/publicdomain/zero/1.0/
+
+
+Sounds in this pack
+-------------------
+
+  * 743978__yellobus__tricky-kick.flac
+    * url: https://freesound.org/s/743978/
+    * license: Creative Commons 0
+  * 743977__yellobus__slick-kick.flac
+    * url: https://freesound.org/s/743977/
+    * license: Creative Commons 0
+  * 743976__yellobus__perfectionist-kick.flac
+    * url: https://freesound.org/s/743976/
+    * license: Creative Commons 0
+  * 743975__yellobus__heavy-kick.flac
+    * url: https://freesound.org/s/743975/
+    * license: Creative Commons 0
+  * 743974__yellobus__city-life-kick.flac
+    * url: https://freesound.org/s/743974/
+    * license: Creative Commons 0
+
+
--- CrabShooter/pythonProject1/code/EntityFactory.py
+++ CrabShooter/pythonProject1/code/EntityFactory.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
+
 from code.Background import Background
-from code.Const import BG_ARRAY, WIN_WIDTH
+from code.Const import BG_ARRAY, WIN_WIDTH, WIN_HEIGHT
+from code.Enemy import Enemy
+from code.Player import Player
 
 
 class EntityFactory:
@@ -9,6 +13,7 @@ class EntityFactory:
     @staticmethod
     def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
+            # criando o background da Fase1
             case 'Level1Bg':
                 list_bg = []
                 for i in range(len(BG_ARRAY)):
@@ -16,3 +21,14 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
 
+            # para instanciar os jogadores
+            case 'Player1':
+                return Player('Player1', (5, WIN_HEIGHT-110))
+            case 'Player2':
+                return Player('Player2', (5, WIN_HEIGHT-210))
+
+            # para instanciar os inimigos
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH + 30, random.randint(0, WIN_HEIGHT)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 130, random.randint(0, WIN_HEIGHT)))
