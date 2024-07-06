9df4245b1b873ad10c5f993dbef91a84ec860b01
Juliano Rodrigues <julianokennedy11@gmail.com>
2024-07-05 22:41:38 -0300

    add classes para controlar o tiro dos elemento. Mecãnica de tiro feita, sem colisões entre tiro/player/inimigo

--- CrabShooter/pythonProject1/code/Const.py
+++ CrabShooter/pythonProject1/code/Const.py
@@ -26,9 +26,13 @@ ENTITY_SPEED = {'Level1Bg0': 0,
                 'Level1Bg3': 3,
                 'Level1Bg4': 4,
                 'Player1': 3,
-                'Player2': 3,
-                'Enemy1': 2,
-                'Enemy2': 1
+                'Player1Shot': 5,
+                'Player2': 2,
+                'Player2Shot': 3,
+                'Enemy1': 4,
+                'Enemy1Shot': 8,
+                'Enemy2': 3,
+                'Enemy2Shot': 6
                 }
 
 ENTITY_HEALTH = {'Level1Bg0': 999,
@@ -36,11 +40,21 @@ ENTITY_HEALTH = {'Level1Bg0': 999,
                  'Level1Bg2': 999,
                  'Level1Bg3': 999,
                  'Level1Bg4': 999,
-                 'Player1': 300,
-                 'Player2': 300,
+                 'Player1': 50,
+                 'Player1Shot': 1,
+                 'Player2': 50,
+                 'Player2Shot': 1,
                  'Enemy1': 100,
-                 'Enemy2': 200
+                 'Enemy1Shot': 2,
+                 'Enemy2': 200,
+                 'Enemy2Shot': 1
                  }
+
+ENTITY_SHOT_DELAY = {'Player1': 20,
+                     'Player2': 10,
+                     'Enemy1': 30,
+                     'Enemy2': 40,
+                     }
 # P
 PLAYER_KEY_UP = {'Player1': pygame.K_UP,
                  'Player2': pygame.K_w}
@@ -53,3 +67,6 @@ PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
 
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
+
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_SPACE}
--- CrabShooter/pythonProject1/code/EntityMediator.py
+++ CrabShooter/pythonProject1/code/EntityMediator.py
@@ -1,5 +1,8 @@
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class EntityMediator:
@@ -7,9 +10,16 @@ class EntityMediator:
     @staticmethod
     def __verify_collision_window(ent: Entity):
         if isinstance(ent, Enemy):
+            inimigo = ent
             if ent.rect.right <= 0:
                 ent.health = 0
-        pass
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >= WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShot):
+            tiro_ini = ent
+            if ent.rect.left <= 0:
+                ent.health = 0
 
     @staticmethod
     def verify_collision(entity_list: list[Entity]):
@@ -22,3 +32,4 @@ class EntityMediator:
         for ent in entity_list:
             if ent.health <= 0:
                 entity_list.remove(ent)
+
